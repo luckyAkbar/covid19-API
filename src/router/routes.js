6faee7ef153633ad462d08f58c85fad50b37d5ae
luckyAkbar <luckyakbar1509@gmail.com>
2021-12-31 12:54:58 +0700

    add new route to '/yearly'

--- src/router/routes.js
+++ src/router/routes.js
@@ -6,6 +6,11 @@ const router = express.Router();
 router.route('/')
   .get(updateHandler);
 
+router.route('/yearly')
+  .get((req, res) => {
+    res.status(200).json({ message: 'yearly' });
+  });
+
 router.route('*')
   .all((req, res) => {
     res.status(404).json({ message: 'Invalid endpoint requested with forbidden method.' });
