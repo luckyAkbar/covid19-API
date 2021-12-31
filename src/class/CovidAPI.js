9fec9d00f6bda98e750f1bd45e417a9e474e54d8
luckyAkbar <luckyakbar1509@gmail.com>
2021-12-31 19:08:51 +0700

    add new method: getYearlyData, _extractYearlyData, _validateTargetYear, _generateEndOfYearDateString

--- src/class/CovidAPI.js
+++ src/class/CovidAPI.js
@@ -39,8 +39,74 @@ class CovidAPI {
     } catch (e) {
       this.ok = false;
       this.status = 503;
-      throw new Error('Service unavailable due to 3rd party API error.');
+      this.message = 'Service unavailable due to 3rd party API error.';
+
+      throw new Error();
     }
+  }
+
+  async getYearlyData(year) {
+    try {
+      const result = await axios.get('https://data.covid19.go.id/public/api/update.json');
+      assert.strictEqual(result.status, 200);
+
+      const { data } = result;
+      const yearlyData = this._extractYearlyData(data, year);
+
+      return {
+        year: this.targetYear,
+        timestamp: yearlyData.key_as_string.value,
+        positive: yearlyData.jumlah_positif_kum.value,
+        recovered: yearlyData.jumlah_sembuh_kum.value,
+        deaths: yearlyData.jumlah_meninggal_kum.value,
+        active: yearlyData.jumlah_dirawat_kum.value,
+      };
+    } catch (e) {
+      this.message = `Yearly Covid19 Case in Indonesia for year ${year} is not found`;
+      this.ok = false;
+      this.status = 404;
+
+      throw e;
+    }
+  }
+
+  _extractYearlyData(fetchResult, year) {
+    this.targetYear = this._validateTargetYear(year);
+    const data = fetchResult.update.harian;
+    const endOfYear = this._generateEndOfYearDateString(this.targetYear);
+
+    for (let i = 0; i < data.length; i++) {
+      if (data[i].key_as_string === endOfYear) {
+        return data[i];
+      }
+    }
+
+    throw new Error();
+  }
+
+  _validateTargetYear(year) {
+    const targetYear = Number(year);
+
+    if (targetYear < 2020) {
+      this.message = `You queried for year ${targetYear}, but since Covid starts detected in indonesia on 2020, we choose to provide the data in first year of Covid pandemic.`;
+      return '2020';
+    }
+
+    if (targetYear > this.currentYear) {
+      this.message = `You queried for year ${targetYear}, because we are unable to predict the future, we choose to provide Covid data until this recent year.`;
+      return `${this.currentYear}`;
+    }
+
+    return year;
+  }
+
+  _generateEndOfYearDateString(targetYear) {
+    if (!targetYear === this.currentYear) return JSON.stringify(new Date(`${targetYear}-12-31`)).replace(/"/g, '');
+
+    const month = this.currentTime.getMonth();
+    const date = this.currentTime.getDate();
+
+    return JSON.stringify(new Date(`${targetYear}-${month}-${date - 1}`)).replace(/"/g, '');
   }
 }
 
