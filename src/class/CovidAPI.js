4c032a58272bdb47b2455b944f119e0b0e7a5e02
luckyAkbar <luckyakbar1509@gmail.com>
2022-01-02 08:54:43 +0700

    create new method: getYearRangedMonthlyData

--- src/class/CovidAPI.js
+++ src/class/CovidAPI.js
@@ -108,6 +108,26 @@ class CovidAPI {
     }
   }
 
+  async getYearRangedMonthlyData(yearParams, querySince, queryUpto) {
+    const { since, upto } = this._extractYearAndMonthFromYearlyRangedMonthlyQuery(yearParams, querySince, queryUpto);
+
+    try {
+      const response = await axios.get('https://data.covid19.go.id/public/api/update.json');
+      assert.strictEqual(response.status, 200);
+
+      const { data } = response;
+      const rangedYearMonthlyData = this._extractMonthlyData(data, since, upto);
+
+      return rangedYearMonthlyData;
+    } catch (e) {
+      this.ok = false;
+      this.message = 'We failed to response to your request due to 3rd party service returning error message.';
+      this.status = 503;
+
+      throw e;
+    }
+  }
+
   _extractRangedYearlyData(fetchResult, since, upto) {
     const yearInBetween = this._generateYearInBetween(since, upto);
     const extractedRangedYearlyData = [];
