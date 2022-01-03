487017d388ee451443a013ad89c35a4ee0adb563
luckyAkbar <luckyakbar1509@gmail.com>
2022-01-03 07:11:18 +0700

    initial commit

---
+++ src/handler/dailyWithYear.js
@@ -0,0 +1,24 @@
+const CovidAPI = require('../class/CovidAPI');
+
+const dailyWithYearHandler = async (req, res) => {
+  const { year } = req.params;
+  const { since, upto } = req.query;
+  const covidAPI = new CovidAPI();
+
+  try {
+    const result = await covidAPI.getDailyWithYearData(year, since, upto);
+
+    res.status(covidAPI.status).json({
+      ok: covidAPI.ok,
+      message: covidAPI.message,
+      data: result,
+    });
+  } catch (e) {
+    this.status(covidAPI.status).json({
+      ok: covidAPI.ok,
+      message: covidAPI.message,
+    });
+  }
+};
+
+module.exports = dailyWithYearHandler;
